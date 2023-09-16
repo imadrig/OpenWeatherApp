d803eb48386c88ff5aff2f92a3244c807e9ae1e3
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-09-16 19:08:01 -0400

    API key working

---
+++ OpenWeatherMap/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+namespace OpenWeatherApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           var client = new HttpClient();
+
+            var key = "7c7e15abba05455a978550ec598cb8d8";
+
+            
+
+            var weatherAppURL = $"https://api.openweathermap.org/data/2.5/weather?lat=38.89&lon=-77.03&appid={key}";
+     
+            var response = client.GetStringAsync(weatherAppURL).Result;  
+
+            Console.WriteLine(response);
+
+            JObject formatted = JObject.Parse(response);
+            //var temp = formatted["list"][0]["main"]["temp"];
+            Console.WriteLine(temp);
+
+        }
+    }
+}
